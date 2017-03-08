c32b7f0a1e51cbaeb21669e75a1d2c9ad37abc7d
inocop <inoue19851110@gmail.com>
2017-03-08 14:01:11 +0900

    fix crop image.

--- src/models/image_crop.php
+++ src/models/image_crop.php
@@ -5,7 +5,7 @@ class image_crop
     private $new_width;
     private $new_height;
 
-    public function __construct($new_width = 250, $new_height = 250)
+    public function __construct($new_width = 200, $new_height = 250)
     {
         require_once(SRC_PATH . 'models/orientaion_fixed.php');
 
@@ -39,7 +39,7 @@ class image_crop
         }
         elseif ($mime == image_type_to_mime_type(IMAGETYPE_PNG))
         {
-            $base_image = imagecreatefromgif($filepath);
+            $base_image = imagecreatefrompng($filepath);
             $new_image = $this->resize_and_crop($base_image);
             imagepng($new_image, $filepath, 9);
         }
@@ -63,26 +63,21 @@ class image_crop
         $base_aspect_ratio = $base_width / $base_height;
         $new_aspect_ratio = $this->new_width / $this->new_height;
 
-        if ($new_aspect_ratio < $base_aspect_ratio){
-            $crop_width = $base_width * ($this->new_height / $base_height);
+        //if ($new_aspect_ratio > $base_aspect_ratio){        // Full image with margin
+        if ($new_aspect_ratio < $base_aspect_ratio){        // Trim image
+            $crop_width = $base_height * $new_aspect_ratio;
             $crop_height = $base_height;
+            $crop_x = (($base_width - $crop_width) / 2);
         }else{
             $crop_width = $base_width;
-            $crop_height = $base_height * ($this->new_width / $base_width);
-        }
-
-        if ($base_width > $base_height) {
-            $crop_width = $base_height;
-            $crop_height = $base_height;
-            $crop_x = ($base_width - $this->new_width) / 2;
-        }
-        else{
-            $crop_width = $base_width;
-            $crop_height = $base_width;
-            $crop_y = ($base_height - $this->new_height) / 2;
+            $crop_height = $base_width / $new_aspect_ratio;
+            $crop_y = (($base_height - $crop_height) / 2);
         }
 
         $new_image = imagecreatetruecolor($this->new_width, $this->new_height);
+        imagealphablending($new_image, false);
+        imagesavealpha($new_image, true);
+
         imagecopyresampled($new_image, $base_image, 0, 0, $crop_x, $crop_y, $this->new_width, $this->new_height, $crop_width, $crop_height);
         imagedestroy($base_image);
 
